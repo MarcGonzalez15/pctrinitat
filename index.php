322d2955dd022b1b84dfe9a3951ed1a51ab0045f
Dasbits <82809727+Dasbits@users.noreply.github.com>
2022-05-19 10:52:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html>
+<title>PCtrinitat</title>
+<head>
+        <link rel="stylesheet" href="index.css" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="header.css">
+        <link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="footer.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+</head>
+                <header class="header-basic">
+                        <div class="header-limiter">
+                                <h1><a href="index.php">PC<span>trinitat</span></a></h1>
+                                <nav>
+                                        <a href="index.php">Inicio</a>
+                                        <a href="productes.php">Productos</a>
+                                        <a href="servicios.php">Servicios</a>
+                                        <a href="contacte.php">Contactos</a>
+                                        <a href="aboutus.php" >About us</a>
+                                </nav>
+                        </div>
+                </header>
+
+
+
+<body1>
+
+
+<!--ZONA PRINCIPAL-->
+    <div id="all_body">
+
+<!--COLUMNA DE LA IZQUIERDA (NUMERO 2 Y 4)-->
+        <div id="body_left">
+
+<!--IMAGEN PRINCIPAL (NUMERO 2)-->
+            <div id="title_img_main"><!--ZONA DE TEXTO ENCIMA DE LA IMAGEN-->
+                <div id="text_title_img_main" style="font-family: 'Courier New', Courier, monospace"><b>Última noticia</b></div>
+            </div>
+
+            <div id="img_main"><!--IMAGEN(SE APLICA TAMAÑO DE DIV Y LA IMG SE PONE DE FONDO)-->
+                <div id="text_img" style="font-family: 'Courier New', Courier, monospace"><b>Apertura de PCtrinitat</b></div><!--TEXTO SOBRE LA IMAGEN-->
+            </div>
+
+            <div id="text_under_img"><!--ZONA DE TEXTO DEBAJO DE LA IMAGEN-->
+                <div id="leters_text_under_img" style="font-family: 'Courier New', Courier, monospace">
+                    <b>Es un honor el dar la noticia de la apretura de nuestra nueva página web, aqui ofreceremos diferentes productos y servicios para el consumidor en el ambito de la informatica, en caso de ayuda no dude contactarnos
+                        </b></div>
+            </div>
+
+            <div id="last_news"><!--ULTIMAS NOTICIAS (NUMERO 4)-->
+
+                <div id="new1">
+                    <div id="new_space1">
+                        <div id="new_info1">
+                            <img id="img_new1" src="https://espacioasesoria.com/wp-content/uploads/2020/09/shutterstock_388786822-501x406.jpg"><!--https://www.emprendedores.es/wp-content/uploads/2021/05/De-emprendedor-a-empresario.jpg25%-->
+                            <p id="text_new1" style="font-family: 'Courier New', Courier, monospace"><b>Empresario implicado en PCtrinitat informa de como han ido los avances con este proyecto para que tenga un buen futuro</b></p><!---->
+                        </div>
+                    </div>
+                </div>
+
+                <div id="new2">
+                    <div id="new_space2">
+                        <div id="new_info2">
+                            <img id="img_new2" src="https://igtbprecision.com/wp-content/uploads/2019/05/Cont%C3%A1ctenos.png">
+                            <p id="text_new2" style="font-family: 'Courier New', Courier, monospace"><b>Aqui en PCtrinitat tenemos un servicio disponible para que puedas ponerte en contacto con nosotros</b></p><!---->
+                        </div>
+                    </div>
+                </div>
+
+                <div id="new3">
+                    <div id="new_space3">
+                        <div id="new_info3">
+                            <img id="img_new3" src="http://www.maritopos.com/imagenes_adic/bg-x4-1.png">
+                            <p id="text_new3" style="font-family: 'Courier New', Courier, monospace"><b>Visita nuestro apartado de Procutos en el que podras obtener periféricos para mejorar tu set-up</b></p><!---->
+                        </div>
+                    </div>
+                </div>
+
+                <div id="new4">
+                    <div id="new_space4">
+                        <div id="new_info4">
+                            <img id="img_new4" src="https://i.ibb.co/NT54V7Z/1652426477338.jpg">
+                            <p id="text_new4" style="font-family: 'Courier New', Courier, monospace"><b>Un programador de PCtrinitat quiere mostrar lo contento que esta trajando en la empresa para la página web</b></p><!---->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+<!--COLUMNA MID-->
+        <div id="columna_mid"></div>
+<!--COLUMNA DERECHA-->
+        <div id="body_right1">
+            <div id="other_news">
+                <div id="title_other_news">
+                    <div id="text_title_other_news" style="font-family: 'Courier New', Courier, monospace">
+                        <b>Otras notícias</b>
+                    </div>
+                </div>
+                <div id="all_news" class="all_news">
+
+                    <div id="all_news1">
+                        <div id="content_news_1" class="content_news">
+                            <a target="_bank" href="https://www.20minutos.es/tecnologia/actualidad/intel-invertira-mas-de-33-000-millones-de-euros-para-producir-chips-en-europa-pero-las-fabricas-no-vendran-a-espana-4971345/" style="font-family: 'Courier New', Courier, monospace">
+                               <b>Intel invertirá más de 33.000 millones de euros para producir chips en Europa, pero las fábricas no vendrán a España</b>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div id="all_news2">
+                        <div id="content_news2" class="content_news">
+                            <a target="_bank" href="https://www.mastekhw.com/noticias/amd-da-mas-detalles-sobre-ryzen-pro-6000/" style="font-family: 'Courier New', Courier, monospace">
+                               <b>AMD da más detalles sobre Ryzen PRO 6000</b>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div id="all_news3">
+                        <div id="content_news2" class="content_news">
+                            <a target="_bank" href="https://www.asus.com/es/news/z2kl9lpr895gvnfx/" style="font-family: 'Courier New', Courier, monospace">
+                               <b>ASUS anuncia soporte BIOS para Ryzen 7 5800X3D y el resto de nuevos procesadores de AMD</b>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div id="all_news4">
+                        <div id="content_news4" class="content_news">
+                            <a target="_bank" href="https://www.noticias3d.com/noticia.asp?idnoticia=88278" style="font-family: 'Courier New', Courier, monospace">
+                                    <b>RYZEN 7 5700X FRENTE A RYZEN 7 5800X Y RYZEN 7 5800X3D</b>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div id="all_news5">
+                        <div id="content_news5" class="content_news">
+                            <a target="_bank" href="https://www.asus.com/es/news/sb3ews3wjzkhhbep/" style="font-family: 'Courier New', Courier, monospace">
+                                <b>ROG lanza la tableta gaming más potente del mundo</b>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div id="all_news6">
+                        <div id="content_news6" class="content_news">
+                            <a target="_bank" href="https://www.asus.com/es/news/fcizvhuqripqnt8r/" style="font-family: 'Courier New', Courier, monospace">
+                                    <b>ASUS lanza el concurso “Mis sueños y yo”, para que los pequeños de la casa den rienda suelta a sus ilusiones</b>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div id="all_news7">
+                        <div id="content_news7" class="content_news">
+                            <a target="_bank" href="https://news.samsung.com/es/samsung-electronics-presenta-la-nueva-era-de-la-tecnologia-micro-led-en-ise-2022" style="font-family: 'Courier New', Courier, monospace">
+                                    <b>Samsung Electronics presenta la nueva era de la tecnología Micro LED en ISE 2022</b>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div id="all_news8">
+                        <div id="content_news8" class="content_news">
+                            <a target="_bank" href="https://news.samsung.com/es/dish-wireless-selecciona-samsung-electronics-para-el-despliegue-de-la-red-de-acceso-de-radio-abierta-5g" style="font-family: 'Courier New', Courier, monospace">
+                                   <b>DISH Wireless selecciona Samsung Electronics para el despliegue de la red de acceso de radio abierta 5G</b>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div id="all_news9">
+                        <div id="content_news9" class="content_news">
+                            <a target="_bank" href="https://www.asus.com/es/news/3pxccl6pb6rq2dbb/" style="font-family: 'Courier New', Courier, monospace">
+                                   <b>ASUS anuncia el nuevo Zenbook 14 Flip OLED (UP5401)</b>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<div id="margen_footer"></div>
+
+
+
+</body1>
+
+
+</body>
+  <!-- FOOTER -->
+  <div class="footer-basic">
+    <footer>
+        <div class="social"><a href="https://www.instagram.com/"><i class="icon ion-social-instagram"></i></a><a href="https://accounts.snapchat.com/"><i class="icon ion-social-snapchat"></i></a><a href="https://twitter.com/"><i class="icon ion-social-twitter"></i></a><a href="https://facebook.com"><i class="icon ion-social-facebook"></i></a></div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="index.php">Inicio</a></li>
+            <li class="li¡t-inline-item"><a href="productes.php">Productos</a></li>
+            <li class="list-inline-item"><a href="servicios.php">Servicios</a></li>
+            <li class="list-inline-item"><a href="contacte.php">Contactanos</a></li>
+            <li class="list-inline-item"><a href="avislegal.php">Aviso legal</a></li>
+        </ul>
+        <p class="copyright">PCtrinitat © 2022</p>
+    </footer>
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+</html>
