79830886a7e955fbe5afea395e7139d962155180
MarcGonzalez15 <105843062+MarcGonzalez15@users.noreply.github.com>
2022-05-20 08:39:01 +0200

    Add files via upload

---
+++ privacidad.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Privacidad</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="header.css">
+	<link rel="stylesheet" href ="privacidad.css">
+	<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="footer.css">
+   	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+</head>
+		<header class="header-basic">
+			<div class="header-limiter">
+				<h1><a href="index.php">PC<span>trinitat</span></a></h1>
+				<nav>
+					<a href="index.php">Inicio</a>
+					<a href="productes.php">Productos</a>
+					<a href="servicios.php">Servicios</a>
+					<a href="contacte.php">Contactos</a>
+					<a href="aboutus.php" >About us</a>
+				</nav>
+			</div>
+		</header>
+<body>
+<h1>Política de privacidad de PCtrinitat</h1>
+<p>Esta Política de privacidad le informa sobre nuestras políticas con respecto a la recopilación, el uso y la divulgación de datos personales cuando interactúa con nuestro sitio web.
+
+Usamos sus datos por varias razones, desde comprender cómo los usuarios interactúan con nuestro sitio web hasta publicar el contenido que lee en nuestro sitio web.
+
+Estas son las principales razones por las que recopilamos y utilizamos datos sobre nuestros usuarios:
+
+Para publicar contenido que sea relevante para nuestros usuarios y para mejorar su experiencia en nuestro sitio web.
+Para llevar a cabo análisis de marketing y estadísticas agregadas.
+Para enviar comunicaciones a nuestros usuarios cuando tengamos su permiso.
+Para permitirnos mostrar publicidad en nuestros sitios.
+Al utilizar nuestro sitio web, usted acepta la recopilación y el uso de información de acuerdo con esta política.</p>
+<h2>Recopilación y uso de información </h2>
+<p>Recopilamos varios tipos diferentes de información para varios propósitos para brindarle y mejorar nuestro Servicio.
+
+No requerimos registro de usuario. Los usuarios son libres de visitar W3Docs y navegar por todas sus páginas. Sin embargo, mientras usa nuestro sitio web, podemos pedirle que nos proporcione cierta información de identificación personal que se puede usar para contactarlo o identificarlo ("Datos personales"). La información de identificación personal puede incluir, pero no se limita a:
+
+Dirección de correo electrónico
+Nombre y apellido
+Cookies y datos de uso </p>
+<h3>Datos de uso </h3>
+<p>También podemos recopilar información sobre cómo se accede y utiliza el Servicio ("Datos de uso"). 
+Estos Datos de uso pueden incluir información como la dirección del Protocolo de Internet de su computadora (por ejemplo, la dirección IP), el tipo de navegador, la versión del navegador, las páginas de nuestro sitio web que visita, la hora y la fecha de su visita, el tiempo dedicado a esas páginas,
+único identificadores de dispositivos y otros datos de diagnóstico. </p>
+
+
+</body>
+<!-- FOOTER -->
+  <div class="footer-basic">
+    <footer>
+        <div class="social"><a href="https://www.instagram.com/"><i class="icon ion-social-instagram"></i></a><a href="https://accounts.snapchat.com/"><i class="icon ion-social-snapchat"></i></a><a href="https://twitter.com/"><i class="icon ion-social-twitter"></i></a><a href="https://facebook.com"><i class="icon ion-social-facebook"></i></a></div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="index.php">Inicio</a></li>
+            <li class="list-inline-item"><a href="productes.php">Productos</a></li>
+            <li class="list-inline-item"><a href="servicios.php">Servicios</a></li>
+            <li class="list-inline-item"><a href="contacte.php">Contactanos</a></li>
+            <li class="list-inline-item"><a href="avislegal.php">Aviso legal</a></li>
+        </ul>
+        <p class="copyright">PCtrinitat © 2022</p>
+    </footer>
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+</html>
