bcecf16309ced2f96aef699ff0d18a891b13d25e
MarcGonzalez15 <105843062+MarcGonzalez15@users.noreply.github.com>
2022-05-20 08:49:07 +0200

    Add files via upload

---
+++ avislegal.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Aviso legal</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="header.css">
+	<link rel="stylesheet" href ="privacidad.css">
+	<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="footer.css">
+   	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+</head>
+		<header class="header-basic">
+			<div class="header-limiter">
+				<h1><a href="index.php">PC<span>trinitat</span></a></h1>
+				<nav>
+					<a href="index.php">Inicio</a>
+					<a href="productes.php">Productos</a>
+					<a href="servicios.php">Servicios</a>
+					<a href="contacte.php">Contactos</a>
+					<a href="aboutus.php" >About us</a>
+				</nav>
+			</div>
+		</header>
+<body>
+<h1>Aviso legal y condiciones de uso</h1>
+
+<h2>Información general</h2>
+<p>En cumplimiento con el deber de información recogido en artículo 10 de la Ley 34/2002, de 11 de julio, de Servicios de la Sociedad de la Información y del Comercio Electrónico,le informamos</p>
+<p>Es una empresa dedicada a la Informática</p>
+<p>PCtrinitat se encuentra inscrita en el Registro Mercantil de Murcia, Tomo 2236, Libro 0, folio 52, hoja MU-52949, inscripción 4</p>
+<p>Email de contacto: pctrinitat@gmail.com</p>
+
+
+<h2>Usuarios</h2>
+<p>El acceso y/o uso de este portal atribuye la condición de USUARIO, que acepta, desde dicho acceso y/o uso, las Condiciones Generales de Uso aquí reflejadas. Las citadas Condiciones serán de aplicación independientemente de las Condiciones Generales de Contratación que en su caso resulten de obligado cumplimiento</p>
+
+
+<h2>Uso del portal</h2>
+<p>PCtrinitat proporciona el acceso a multitud de productos, informaciones, servicios, programas o datos (en adelante, "los contenidos") en Internet pertenecientes a PCtrinitat, o a terceros a los que el USUARIO puede tener acceso. El USUARIO asume la responsabilidad del uso del portal. Dicha responsabilidad se extiende al registro que fuese necesario para acceder a determinados servicios o contenidos. En dicho registro el USUARIO será responsable de aportar información veraz y lícita. Como consecuencia de este registro, al USUARIO se le puede proporcionar una contraseña de la que será responsable, comprometiéndose a hacer un uso diligente y confidencial de la misma. El USUARIO se compromete a hacer un uso adecuado de los contenidos y servicios que PCtrinitat ofrece a través de su portal y con carácter enunciativo pero no limitativo, a no emplearlos para:</p>
+<p>· Incurrir en actividades ilícitas, ilegales o contrarias a la buena fe y al orden público</p>
+<p>· Difundir contenidos o propaganda de carácter racista, xenófobo, pornográfico ilegal, de apología del terrorismo o atentatorio contra los derechos humanos</p>
+<p>· Provocar daños en los sistemas físicos y lógicos de PCtrinitat, de sus proveedores o de terceras personas, introducir o difundir en la red virus informáticos o cualesquiera otros sistemas físicos o lógicos que sean susceptibles de provocar los daños anteriormente mencionados</p>
+<p>· Intentar acceder y, en su caso, utilizar las cuentas de correo electrónico de otros usuarios y modificar o manipular sus mensajes</p>
+
+
+<h2>Propiedad Intelectual e Industrial</h2>
+<p>1. Todos los signos distintivos, marcas, nombres comerciales, contenidos, estructura, diseño y forma de presentación de los elementos y cualquier otra información que aparezca en este sitio Web son propiedad de PCtrinitat por sí o como cesionaria y están protegidos por los derechos de propiedad industrial e intelectual</p>
+<p>2. El usuario tiene prohibida la reproducción, transformación, distribución, comunicación pública y, en general cualquier otra forma de explotación de los elementos referidos en el apartado anterior sin autorización expresa de PCtrinitat</p>
+<p>3. El usuario se abstendrá de emplear medios que puedan suprimir, alterar, eludir o manipular cualesquiera dispositivos de protección o sistemas de seguridad que puedan estar instalados y que comporten un riesgo o daño o inutilización del sitio Web y/o sus contenidos</p>
+<p>4. PCtrinitat no se responsabiliza del posible uso inapropiado que terceros realicen de esta página Web, ni de la información que a través de ella transmitan a terceros. El uso de los contenidos que pueda hacer el usuario y las eventuales consecuencias, daños o perjuicios que pudiesen derivarse, son de la exclusiva responsabilidad del usuario. PCtrinitat se excluye por los daños y perjuicios de toda naturaleza causados a los usuarios por el uso de enlaces (links), directorios y herramientas de búsqueda, que permiten a los usuarios acceder a sitios Web pertenecientes y/o gestionados por terceros así como de la presencia de virus u otros códigos maliciosos en los contenidos que puedan producir cualquier tipo de daños en el sistema informático, documentos electrónicos o ficheros de los usuarios. PCtrinitat se reserva el derecho de ejercitar las acciones legales que considere oportunas derivadas de cualesquiera usos ilícitos por parte de terceros de los contenidos de su página web</p>
+
+
+<h2>Exclusión de garantías y responsabilidad</h2>
+<p>PCtrinitat no se hace responsable, en ningún caso, de los daños y perjuicios de cualquier naturaleza que pudieran ocasionar, a título enunciativo: errores u omisiones en los contenidos, falta de disponibilidad del portal o la transmisión de virus o programas maliciosos o lesivos en los contenidos, a pesar de haber adoptado todas las medidas tecnológicas necesarias para evitarlo</p>
+
+
+<h2>Modificaciones</h2>
+<p>PCtrinitat se reserva el derecho de efectuar sin previo aviso las modificaciones que considere oportunas en su portal, pudiendo cambiar, suprimir o añadir tanto los contenidos y servicios que se presten a través de la misma como la forma en la que éstos aparezcan presentados o localizados en su portal</p>
+
+
+<h2>Enlaces</h2>
+<p>En el caso de que en nombre del dominio se dispusiesen enlaces o hipervínculos hacía otros sitios de Internet, PCtrinitat no ejercerá ningún tipo de control sobre dichos sitios y contenidos. En ningún caso PCtrinitat asumirá responsabilidad alguna por los contenidos de algún enlace perteneciente a un sitio web ajeno, ni garantizará la disponibilidad técnica, calidad, fiabilidad, exactitud, amplitud, veracidad, validez y constitucionalidad de cualquier material o información contenida en ninguno de dichos hipervínculos u otros sitios de Internet. Igualmente la inclusión de estas conexiones externas no implicará ningún tipo de asociación, fusión o participación con las entidades conectadas</p>
+<p>PCtrinitat se reserva el derecho a denegar o retirar el acceso a portal y/o los servicios ofrecidos sin necesidad de preaviso, a instancia propia o de un tercero, a aquellos usuarios que incumplan las Condiciones Generales de Uso</p>
+
+
+<h2>Derecho de exclusión</h2>
+<p>PCtrinitat se reserva el derecho a denegar o retirar el acceso a portal y/o los servicios ofrecidos sin necesidad de preaviso, a instancia propia o de un tercero, a aquellos usuarios que incumplan las Condiciones Generales de Uso</p>
+
+
+<h2>Generalidades</h2>
+<p>PCtrinitat perseguirá el incumplimiento de las condiciones así como cualquier utilización indebida de su portal ejerciendo todas las acciones civiles y penales que le puedan corresponder en derecho</p>
+
+
+<h2>Modificación de las presentes condiciones y duración</h2>
+<p>PCtrinitat podrá modificar en cualquier momento las condiciones aquí determinadas, siendo debidamente publicadas como aquí aparecen</p>
+
+
+<h2>Legislación aplicable y jurisdicción</h2>
+<p>La relación entre PCtrinitat y el USUARIO se regirá por la normativa española vigente y cualquier controversia se someterá a los Juzgados y Tribunales españoles</p>
+<p>Resolución de litigios en línea en materia de consumo conforme al Art. 14.1 del Reglamento (UE) 524/2013: La Comisión Europea facilita una plataforma de resolución de litigios en línea que se encuentra disponible en el siguiente enlace: </p>
+<a target="_bank" href="http://ec.europa.eu/consumers/odr/" style="color:black">http://ec.europa.eu/consumers/odr/</a>
+
+
+</body>
+<!-- FOOTER -->
+  <div class="footer-basic">
+    <footer>
+        <div class="social"><a href="https://www.instagram.com/"><i class="icon ion-social-instagram"></i></a><a href="https://accounts.snapchat.com/"><i class="icon ion-social-snapchat"></i></a><a href="https://twitter.com/"><i class="icon ion-social-twitter"></i></a><a href="https://facebook.com"><i class="icon ion-social-facebook"></i></a></div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="index.php">Inicio</a></li>
+            <li class="list-inline-item"><a href="productes.php">Productos</a></li>
+            <li class="list-inline-item"><a href="servicios.php">Servicios</a></li>
+            <li class="list-inline-item"><a href="contacte.php">Contactanos</a></li>
+	    <li class="list-inline-item"><a href="avislegal.php">Aviso legal</a></li>
+        </ul>
+        <p class="copyright">PCtrinitat © 2022</p>
+    </footer>
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+</html>
