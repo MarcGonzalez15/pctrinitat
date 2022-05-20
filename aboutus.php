e62c68bfe4f4c26a53ffb9352cbd337e323fd10e
MarcGonzalez15 <105843062+MarcGonzalez15@users.noreply.github.com>
2022-05-20 08:45:54 +0200

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<head>
+    <title>About us</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="header.css">
+	<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="aboutus.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+</head>
+<header class="header-basic">
+    <div class="header-limiter">
+        <h1><a href="index.php">PC<span>trinitat</span></a></h1>
+        <nav>
+            <a href="index.php">Inicio</a>
+            <a href="productes.php">Productos</a>
+            <a href="servicios.php">Servicios</a>
+            <a href="contacte.php">Contactos</a>
+            <a href="aboutus.php" >About us</a>
+        </nav>
+    </div>
+</header>
+<body>
+    <h2>WHO WE ARE</h2>
+    <p>PCtrinitat is a new company on the market that pretends selling products for gamming and office.<br>We want our clients to be satisfied with our services and atention.
+    <h4>CEO: Pol Terres</h4><br><p>Pol Terres is the actual CEO of the company directs and supervise the actual projects that PCtrinitat is developing.</p>
+    <h4>Marketing director:Marc González</h4><br><p>Marc gonzález is responsable of the great seeing it has the webpage and its design.</p>
+    <h4>General technician:David Salvador</h4><br><p>David does all the dirty work, he is the programmer behind this webpage.</p>
+    </p>
+    <h2>WHAT ABOUT THE FUTURE?</h2>
+    <p>PCtrinitat wants to implement more services for our customers, we have planned to do a new category called professional help<br>PCtrinitat's team is happy to be helping you with your webpage or informatic request this option will be ONLY for companies. </p>
+</body>
+  <!-- FOOTER -->
+  <div class="footer-basic">
+    <footer>
+	<div class="social"><a href="https://www.instagram.com/"><i class="icon ion-social-instagram"></i></a><a href="https://accounts.snapchat.com/"><i class="icon ion-social-snapchat"></i></a><a href="https://twitter.com/"><i class="icon ion-social-twitter"></i></a><a href="https://facebook.com"><i class="icon ion-social-facebook"></i></a></div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="index.php">Inicio</a></li>
+            <li class="list-inline-item"><a href="productes.php">Productos</a></li>
+            <li class="list-inline-item"><a href="servicios.php">Servicios</a></li>
+            <li class="list-inline-item"><a href="contacte.php">Contactanos</a></li>
+            <li class="list-inline-item"><a href="avislegal.php">Aviso legal</a></li>
+        </ul>
+        <p class="copyright">PCtrinitat © 2022</p>
+    </footer>
+  </div>
+</html>
