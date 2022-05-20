229c03220f18425be3b20783c020d52c2725b61c
MarcGonzalez15 <105843062+MarcGonzalez15@users.noreply.github.com>
2022-05-20 08:35:52 +0200

    Add files via upload

---
+++ servicios.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML>
+<head>
+	<title>Servicios</title>
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="header.css">
+	<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+    	<link rel="stylesheet" href="servicios.css">
+	<link rel="stylesheet" href="footer.css">
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+</head>
+<header class="header-basic">
+    <div class="header-limiter">
+        <h1><a href="index.php">PC<span>trinitat</span></a></h1>
+        <nav>
+            <a href="index.php">Inicio</a>
+            <a href="productes.php">Productos</a>
+            <a href="servicios.php">Servicios</a>
+            <a href="contacte.php">Contactos</a>
+	    <a href="aboutus.php" >About us</a>
+        </nav>
+    </div>
+</header>
+<body>
+<div class="container">
+    	<div id="wrapper">
+        	<div id="content">
+	    		<a class="button" href="contacte.php">Contactanos </a>
+           		<span id="titl">PCTRINITAT</span>
+            		<p><strong>Servicios que ofrece PCtrinitat</strong></p>
+            		<h3>Asistencia tecnica </h3>
+            		<p>PCtrinitat te ayuda en cualquier problema o pregunta que tengas sobre nuestra empresa y/o productos 
+            		</p>
+            		<p>Además te damos la posibilidad de poder ver tus pedidos para saber cuanto llegaran en tardar y que ha comprado
+            		</p>
+            		<h3>Trabja con nosotros</h3>
+            		<p>PCtrinitat quiere contratar a personas para trabajar con nosotros, en caso de estar interesado contactanos mediante nuestro formulario
+            		</p>
+       		</div>
+    	</div>
+</div>
+</body>
+<!-- FOOTER -->
+<div class="footer-basic">
+    <footer>
+        <div class="social"><a href="https://www.instagram.com/"><i class="icon ion-social-instagram"></i></a><a href="https://accounts.snapchat.com/"><i class="icon ion-social-snapchat"></i></a><a href="https://twitter.com/"><i class="icon ion-social-twitter"></i></a><a href="https://facebook.com"><i class="icon ion-social-facebook"></i></a></div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="index.php">Inicio</a></li>
+            <li class="list-inline-item"><a href="productes.php">Productos</a></li>
+            <li class="list-inline-item"><a href="servicios.php">Servicios</a></li>
+            <li class="list-inline-item"><a href="contacte.php">Contactanos</a></li>
+            <li class="list-inline-item"><a href="avislegal.php">Aviso legal</a></li>
+        </ul>
+        <p class="copyright">PCtrinitat © 2022</p>
+    </footer>
+ </div>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+</html>
